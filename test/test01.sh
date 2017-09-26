70cb323fd47ea23778e460201b88187413410abe
T420 <0937160git@gmail.com>
2017-09-26 18:38:56 +0100

    用 shell-format 調整一下 code sytle

--- test/test01.sh
+++ test/test01.sh
@@ -5,7 +5,7 @@
 # >/dev/null 2>&1
 
 test_check_ID_usability() {
-    
+
     check_ID_usability
     assertTrue '無輸入驗證失敗' "[ $? -gt 0 ]"
 
@@ -19,15 +19,15 @@ test_check_ID_usability() {
     assertTrue '輸入 q 無法退出。' "[ $? -eq 2 ]"
 
     check_ID_usability Q
-    assertTrue '輸入 q 無法退出。' "[ $? -eq 2 ]"    
+    assertTrue '輸入 q 無法退出。' "[ $? -eq 2 ]"
 
 }
 
 oneTimeSetUp() {
-  # load include to test
-  . ../r1_check_unsuccessful_login_count.sh
-  assertTrue 'Log檔不存在。' "[ -r $LOGFILENAME ]"
-  assertTrue 'ErrorLog檔不存在。' "[ -r $ERRFILENAME ]"
+    # load include to test
+    . ../r1_check_unsuccessful_login_count.sh
+    assertTrue 'Log檔不存在。' "[ -r $LOGFILENAME ]"
+    assertTrue 'ErrorLog檔不存在。' "[ -r $ERRFILENAME ]"
 }
 
 . ../shunit2/shunit2
